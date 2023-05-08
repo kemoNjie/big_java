a74ff02ba1f1c0db822476c7c3d42e57f2ecf083
Kemo Njie <kemonjie2@gmail.com>
2023-05-08 23:53:33 +0000

    working on the baiscs

--- src/introduction/Main.java
+++ src/introduction/Main.java
@@ -1,2 +1,16 @@
-package introduction;public class Main {
+package introduction;
+import java.util.Scanner;
+/**
+ * @author kemo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        var in  = new Scanner(System.in);
+        System.out.print("Enter you name: ");
+        String name =  in.next();
+
+        System.out.println("Hello, "+name);
+    }
 }
